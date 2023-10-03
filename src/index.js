9e09d484e0acc5de1f86a4cbcf9f6f058394514c
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-10-04 01:31:01 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -1,51 +1,66 @@
-import { fetchBreedImgs, fetchBreeds } from './js/cat-api';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
-import Notiflix from 'notiflix';
+// Import necessary modules and CSS
+import SlimSelect from 'slim-select'; // Import SlimSelect for dropdown
+import 'slim-select/dist/slimselect.css'; // Import SlimSelect CSS
+import Notiflix from 'notiflix'; // Import Notiflix for notifications
+import { fetchBreedImgs, fetchBreeds } from './js/cat-api'; // Import your API functions
 
 
-
+// Select DOM elements
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const errorElement = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
+// Initialize SlimSelect for the breed selection dropdown
 const slim = new SlimSelect(breedSelect, {
   placeholder: 'Select a breed',
 });
 
+// Add a change event listener to the breed selection dropdown
 breedSelect.addEventListener('change', OnSelectChange);
 
+// Hide the loader initially
 loader.classList.remove('visually-hidden');
 
+// Function to handle breed selection change
 function OnSelectChange(e) {
+    // Get the selected breed ID from the dropdown
     const breedsId = e.target.value;
 
+    // Hide elements and show loader while fetching data
     breedSelect.classList.add('visually-hidden');
     loader.classList.remove('visually-hidden');
     errorElement.classList.add('visually-hidden');
     catInfo.classList.add('visually-hidden');
     
+    // Fetch breed images for the selected breed
     fetchBreedImgs(breedsId)
         .then(data => {
+           // Check if data has images
            if (data.length > 0) {
                 const { url } = data[0];
                 const breedInfo = data[0].breeds[0];
                 const { name, description, temperament } = breedInfo;
 
+                // Create HTML markup for displaying cat information
                 const markup = `
-                <img src="${url}" alt="${name}"}/>
+                <img src="${url}" alt="${name}"/>
                 <h3>${name}</h3>
                 <p>${description}</p>
                 <p>${temperament}</p>`;
 
+                // Update the catInfo section with the markup
                 catInfo.innerHTML = markup;
 
+                // Show the breed selection and hide the loader and error
                 breedSelect.classList.remove('visually-hidden');
                 loader.classList.add('visually-hidden');
                 catInfo.classList.remove('visually-hidden');
             } else {
+                // Notify the user if no images were found for the breed
                 Notiflix.Notify.failure('No images found for this breed.');
+
+                // Show the breed selection and hide the loader and error
                 breedSelect.classList.remove('visually-hidden');
                 loader.classList.add('visually-hidden');
                 errorElement.classList.add('visually-hidden');
@@ -53,11 +68,14 @@ function OnSelectChange(e) {
             }
         })
         .catch(error => {
+            // Handle errors by showing a notification to the user
             Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
                 closeButton: true,
                 cssAnimationStyle: 'from-top',
                 timeout: 3000,
             });
+
+            // Show the breed selection and hide the loader and error
             breedSelect.classList.remove('visually-hidden');
             loader.classList.add('visually-hidden');
             errorElement.classList.add('visually-hidden');
@@ -65,25 +83,32 @@ function OnSelectChange(e) {
         });
 }
 
+// Fetch and populate the breed selection dropdown initially
 fetchBreeds()
     .then(res => {
+        // Create the HTML markup for breed options and populate the dropdown
         const markup = res.map(el => {
             return `<option value="${el.id}">${el.name}</option>`;
         }).join("");
 
         breedSelect.innerHTML = markup;
 
+        // Hide the loader and show the breed selection
         loader.classList.add('visually-hidden');
         breedSelect.classList.remove('visually-hidden');
 
     }).catch(error => {
+        // Handle errors by showing a notification to the user
         Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
             closeButton: true,
             cssAnimationStyle: 'from-top',
             timeout: 3000,
         });
-        errorElement.classList.add('visually-hidden');
+
+        // Hide the error element and breed selection
+        errorElement.classList.remove('visually-hidden');
         breedSelect.classList.add('visually-hidden');
     }).finally(() => {
+        // Hide the loader after fetching breeds
         loader.classList.add('visually-hidden');
     });
